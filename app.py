19fedfc6dde082b7114e1c9d0e9d954379e4737a
Prakeh-ai <22f3002638@ds.study.iitm.ac.in>
2025-06-14 21:37:25 +0530

    saving local work before pull

--- app.py
+++ app.py
@@ -724,7 +724,4 @@ async def health_check():
             content={"status": "unhealthy", "error": str(e), "api_key_set": bool(API_KEY)}
         )
 
-if __name__ == "__main__":
-    #uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+#adding comment
