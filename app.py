4ba6c39e98053ef4c2d95c7b5fe34d7adf5d4b03
Prakeh-ai <22f3002638@ds.study.iitm.ac.in>
2025-06-14 19:40:16 +0530

    Changed startup logic

--- app.py
+++ app.py
@@ -725,4 +725,6 @@ async def health_check():
         )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True) 
+    #uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
