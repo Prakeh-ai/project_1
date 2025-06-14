554fb148800183fd6fc943b9f1178314ecda0003
Prakeh-ai <22f3002638@ds.study.iitm.ac.in>
2025-06-14 22:09:55 +0530

    Final

--- app.py
+++ app.py
@@ -22,10 +22,8 @@ from dotenv import load_dotenv
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-# Constants
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # path to /api
-DB_PATH = os.path.join(BASE_DIR, "../knowledge_base.db")
-#DB_PATH = "../knowledge_base.db"
+
+DB_PATH = "../knowledge_base.db"
 SIMILARITY_THRESHOLD = 0.68  # Lowered threshold for better recall
 MAX_RESULTS = 10  # Increased to get more context
 load_dotenv()
